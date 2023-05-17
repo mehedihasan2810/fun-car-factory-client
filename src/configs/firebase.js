1d065f1afc73153f743d30f214a7cc082ce2b82b
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-05-17 23:06:45 +0600

    shifted firebase configuration to env

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,16 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAGWxkWekGd8RTPRzbQbhB45dbDkiPxf7c",
-  authDomain: "fun-car-factory.firebaseapp.com",
-  projectId: "fun-car-factory",
-  storageBucket: "fun-car-factory.appspot.com",
-  messagingSenderId: "466540186149",
-  appId: "1:466540186149:web:865a593747e728cdc74dcc"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+
+  //   apiKey: "AIzaSyAGWxkWekGd8RTPRzbQbhB45dbDkiPxf7c",
+  //   authDomain: "fun-car-factory.firebaseapp.com",
+  //   projectId: "fun-car-factory",
+  //   storageBucket: "fun-car-factory.appspot.com",
+  //   messagingSenderId: "466540186149",
+  //   appId: "1:466540186149:web:865a593747e728cdc74dcc"
 };
 
 // Initialize Firebase
