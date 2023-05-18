c2603fb8d1bbed853138497cf3246838a3d60892
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-05-18 22:08:47 +0600

    added signin page

--- src/pages/AddToy/AddToy.jsx
+++ src/pages/AddToy/AddToy.jsx
@@ -1,8 +1,18 @@
 import "./AddToy.css";
 const AddToy = () => {
   return (
-    <div className="add-toy-container">
+    <div className="add-toy-container center-container">
       <form>
+        <div className="row">
+          <div className="control">
+            <label htmlFor="name">Toy Name: </label>
+            <input type="text" name="name" id="name" placeholder="Toy Name" />
+          </div>
+          <div className="control">
+            <label htmlFor="text">text: </label>
+            <input type="text" name="text" id="text" placeholder="text" />
+          </div>
+        </div>
         <div className="row">
           <div className="control">
             <label htmlFor="name">Toy Name: </label>
