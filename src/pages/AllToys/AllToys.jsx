e6d365e31ad2d0f4e93a8790bad26777ccbed93f
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-05-18 23:00:30 +0600

    added all toys page

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import "./AllToys.css";
 const AllToys = () => {
   return (
-    <div>AllToys</div>
-  )
-}
+    <div className="center-container">
+      <div className="all-toys-container">
+        <table>
+          <tr>
+            <th>Seller Name</th>
+            <th>Toy Name</th>
+            <th>Sub Category</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Button</th>
+          </tr>
+          <tr>
+            <td>Jill</td>
+            <td>Tesla Super Truck</td>
+            <td>truck</td>
+            <td>20$</td>
+            <td>5</td>
+            <td><button className="btn-primary">View Details</button></td>
+          </tr>
+          <tr>
+            <td>Jill</td>
+            <td>Tesla Super Truck</td>
+            <td>truck</td>
+            <td>20$</td>
+            <td>5</td>
+            <td><button className="btn-primary">View Details</button></td>
+          </tr>
+          <tr>
+            <td>Jill</td>
+            <td>Tesla Super Truck</td>
+            <td>truck</td>
+            <td>20$</td>
+            <td>5</td>
+            <td><button className="btn-primary">View Details</button></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default AllToys
+export default AllToys;
