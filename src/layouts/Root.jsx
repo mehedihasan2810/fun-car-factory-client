e6b1271dc650187c06e5c8258a9e22d0447e0240
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-08-24 16:06:58 +0600

    added texture background

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import { ToastContainer } from "react-toastify";
+import './Root.css'
 
 const Root = () => {
   return (
@@ -10,6 +11,11 @@ const Root = () => {
       <Outlet />
       <Footer />
       <ToastContainer />
+      <div className="fixed-background">
+        <div className="texture"></div>
+        <div className="dark-texture-layer"></div>
+      </div>
+      {/* <img className="texture-bg-img"  src="../../public/assets/texture-bg2.avif" alt="texture background image" /> */}
     </main>
   );
 };
