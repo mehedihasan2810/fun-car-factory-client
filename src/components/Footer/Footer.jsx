3040165f39c4bdc00926e96e8c5d77e339021d71
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-09-16 23:42:48 +0600

    footer new version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,66 +2,52 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 import "./Footer.css";
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <footer className="footer">
-        <div className="top">
-          <div className="item">
-            <h1>Categories</h1>
-            <span>Truck Toys</span>
-            <span>Ferrari Toys</span>
-            <span>Volvo Toys</span>
-            <span>BMW Toys</span>
-            <span>Best Rated</span>
-          </div>
-          <div className="item">
-            <h1>Links</h1>
-            <span>FAQ</span>
-            <span>Pages</span>
-            <span>Stories</span>
-            <span>Compare</span>
-            <span>Cookies</span>
-          </div>
-          <div className="item">
-            <h1>About</h1>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde enim
-              eos vitae blanditiis neque molestias itaque sequi, voluptatem
-              voluptatum, error placeat magni? Rerum mollitia dignissimos quo
-              magnam culpa quasi maiores.
-            </span>
-          </div>
-          <div className="item">
-            <h1>Contact</h1>
-            <p>Phone: O172*******</p>
-            <p>12 Avenue, Dhaka, Bangladesh</p>
-            <div className="social">
-              <FaFacebook/>
-              <FaInstagram/>
-              <FaLinkedin/>
-              <FaTwitter/>
-            </div>
-            {/* <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-              minus dolore tempora beatae modi sequi eligendi delectus, ea sed
-              voluptate itaque similique quos doloremque quam aut numquam
-              maxime. Officiis, atque.
-            </span> */}
+    <footer className="footer">
+      <div className="top">
+        <div className="item">
+          <h6>Categories</h6>
+          <span>Truck Toys</span>
+          <span>Ferrari Toys</span>
+          <span>Volvo Toys</span>
+          <span>BMW Toys</span>
+          <span>Best Rated</span>
+        </div>
+        <div className="item">
+          <h6>Links</h6>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stories</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h6>About</h6>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde enim
+            eos vitae blanditiis neque molestias itaque sequi, voluptatem
+            voluptatum, error placeat magni? Rerum mollitia dignissimos quo
+            magnam culpa quasi maiores.
+          </span>
+        </div>
+        <div className="item">
+          <h6>Contact</h6>
+          <span>spanhone: O172*******</span>
+          <span>12 Avenue, Dhaka, Bangladesh</span>
+          <div className="social">
+            <FaFacebook />
+            <FaInstagram />
+            <FaLinkedin />
+            <FaTwitter />
           </div>
         </div>
-        <div className="bottom">
-          <div className="left">
+      </div>
+      <div className="bottom">
+        <div className="left">
           <img src="/assets/logo.png" alt="" />
-            <span className="logo">FunCarFactory</span>
-            <span className="copyright">
-              Copyright 2023. All Rights Reserved
-            </span>
-          </div>
-          {/* <div className="right">
-        <img src="./images/payment-gateway.png" alt="payment gateway" />
-      </div> */}
+          <span className="copyright">Copyright 2023. All Rights Reserved</span>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
