6dd2a75e9b453f218688707058548c53fc84c4ce
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-09-25 13:46:52 +0600

    added search bar in the toys page

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -31,6 +31,32 @@ const AllToys = () => {
         <div className="toys-total-count">
           Toys <span>({allToys.length})</span>
         </div>
+        {/* <form> */}
+          <div className="search-form-control">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+
+            <input
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search Your Favorite Toys..."
+            />
+            <button>Search</button>
+          </div>
+        {/* </form> */}
         <div className="toys-filter-sortby-wrapper">
           <div className="toys-filter-wrapper">
             <button>
