d86bc17e0675251e0b74e04236c5ed8bd612db93
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-10-05 23:20:38 +0600

    configured apollo InMemoryCache for getCar

--- src/lib/graphql/index.js
+++ src/lib/graphql/index.js
@@ -1,6 +1,19 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        getCar(_, { args, toReference }) {
+          return toReference({
+            __typename: "Car",
+            id: args.id,
+          });
+        },
+      },
+    },
+  },
+});
 
 export const apolloClient = new ApolloClient({
   uri: "https://fun-car-factory-server.vercel.app/graphql",
