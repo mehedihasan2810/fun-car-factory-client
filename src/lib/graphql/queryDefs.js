b95705979e5e5f59112067aa89e1f85a187fe3bc
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-10-09 22:57:28 +0600

    added user query and mutation typedefs

--- src/lib/graphql/queryDefs.js
+++ src/lib/graphql/queryDefs.js
@@ -29,3 +29,58 @@ export const GET_CAR = gql`
     }
   }
 `;
+
+export const CREATE_CAR = gql`
+  mutation CreateCar($carInput: CarInput!) {
+    createCar(carInput: $carInput) {
+      code
+      message
+      success
+      car {
+        id
+        category
+        description
+        email
+        name
+        price
+        quantity
+        rating
+        sellerName
+        url
+      }
+    }
+  }
+`;
+
+export const GET_USERS = gql`
+  query GetUsers {
+    getUsers {
+      email
+      id
+      name
+      role
+    }
+  }
+`;
+
+export const GET_USER = gql`
+  query GetUser($email: String!) {
+    getUser(email: $email) {
+      email
+      id
+      name
+      role
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation CreateUser($input: UserInput!) {
+    createUser(input: $input) {
+      email
+      id
+      name
+      role
+    }
+  }
+`;
