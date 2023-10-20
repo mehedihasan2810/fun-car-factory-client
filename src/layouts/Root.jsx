4621d8f6b295216933f215ba714e2bcb0563a6f9
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-10-20 22:27:13 +0600

    added initial loading page

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -3,10 +3,30 @@ import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import { ToastContainer } from "react-toastify";
 import "./Root.css";
+import { useEffect, useRef } from "react";
 
 const Root = () => {
+  const loadingElRef = useRef();
+
+  useEffect(() => {
+    const initialLoad = () => {
+      console.log("loaded");
+      loadingElRef.current.style.transform = `translateY(-100%)`;
+    };
+    window.addEventListener("load", initialLoad);
+
+    return () => {
+      window.removeEventListener("load", initialLoad);
+    };
+  }, []);
+
   return (
     <>
+      <div ref={loadingElRef} className="initial-loading-container">
+        <div>
+          Fun Car Fact<span className="initial-loader"></span>ry
+        </div>
+      </div>
       <Navbar />
       <main>
         <Outlet />
